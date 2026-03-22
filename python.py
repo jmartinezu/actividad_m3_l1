eba81c37e0e0f95b64b39cd223c6b3749612226b
jmartinezu <96530601+jmartinezu@users.noreply.github.com>
2026-03-22 18:46:26 +0000

    python 1

---
+++ python.py
@@ -0,0 +1,22 @@
+# 1. Imprimir un mensaje inicial
+print("¡Hola! Este es mi primer programa en Python.")
+
+# 2. Declarar variables simples
+nombre = "Joaquín Martínez"
+edad = 31
+ciudad = "Talagante"
+
+print("Nombre:", nombre)
+print("Edad:", edad)
+print("Ciudad:", ciudad)
+
+# 3. Realizar operaciones básicas
+numero_a = 15
+numero_b = 25
+resultado = numero_a + numero_b # Sumamos las variables
+print("El resultado de la suma es:", resultado)
+
+# 4. Explorar un módulo integrado
+import math
+raiz = math.sqrt(25) # Calculamos la raíz cuadrada
+print("La raíz cuadrada de 25 es:", raiz)
